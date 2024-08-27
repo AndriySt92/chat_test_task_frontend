be29c01bc529a44c5931c36069db677d059a0f4d
AndriySt92 <andriyua-1992@mail.ru>
2024-08-27 11:39:14 +0300

    Add pre-deployment configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/chat_test_task_frontend/',  // Replace with your GitHub repo name
+  build: {
+    outDir: 'dist',
+  },
   server: {
     open: true,
   },
